6a23fa1a9a62a2acd67c4b4b74f28a85b01d07f1
Mia Hall <mia.hall@agilitypr.com>
2019-09-24 12:56:10 -0400

    Getting branches on init

--- src/tree/scm/branches.ts
+++ src/tree/scm/branches.ts
@@ -1,22 +1,57 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
+import { GitExtension, Repository } from '../../typings/git';
+import { promisify } from 'util';
+import * as child_process from 'child_process';
 
+const exec = promisify(child_process.exec);
 interface Branch {
-    id: string;
+    branchName: string;
 }
 
 export class BranchTreeProvider implements vscode.TreeDataProvider<Branch> {
+    private repos: Repository[] = [];
+
+    constructor() {
+        const gitContainer = vscode.extensions.getExtension<GitExtension>('vscode.git');
+        if (gitContainer) {
+            const gitExtension = gitContainer.exports;
+            const git = gitExtension.getAPI(1);
+            this.repos = git.repositories;
+        }
+    }
+
+    public async getBranches(repo: Repository): Promise<string[]> {
+        const path = repo.rootUri.fsPath;
+        if (!path) {
+            return [];
+        }
+        const {stdout} = await exec(
+            'git branch',
+            {
+                cwd: path
+            }
+        );
+        const branches = stdout.replace(/^\*\s+/g, '').split(/\n/g).filter(branch => !!branch);
+        return branches;
+    }
 
     getTreeItem(element: Branch): vscode.TreeItem {
-        return new vscode.TreeItem(element.id);
+        return new vscode.TreeItem(element.branchName);
     }
-    getChildren(element: Branch): Branch[] {
+    async getChildren(element: Branch): Promise<Branch[]> {
+        if (!this.repos) {
+            return [];
+        }
         if (!element) {
-            return [
-                {id: 'master'},
-                {id: 'other-branch'}
-            ];
+            // root level
+            if (this.repos.length === 1) {
+                const repo = this.repos[0];
+                const branches = await this.getBranches(repo);
+
+                return branches.map(branchName => <Branch>{branchName: branchName});
+            }
         }
         return [];
     }
