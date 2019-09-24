1b359305c8051efc19449ab959bfe2878cf66a9b
Mia Hall <mia.hall@agilitypr.com>
2019-09-24 14:17:37 -0400

    Refresh branch list

--- src/tree/scm/branches.ts
+++ src/tree/scm/branches.ts
@@ -1,28 +1,69 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
-import { GitExtension, Repository } from '../../typings/git';
+import { GitExtension, Repository, API } from '../../typings/git';
 import { promisify } from 'util';
 import * as child_process from 'child_process';
 
 const exec = promisify(child_process.exec);
 interface Branch {
-    branchName: string;
+    repo: Repository;
+    branchName?: string;
+    selected?: boolean;
 }
 
 export class BranchTreeProvider implements vscode.TreeDataProvider<Branch> {
+    private gitApi?: API;
     private repos: Repository[] = [];
 
+    private _onDidChangeTreeData: vscode.EventEmitter<any> = new vscode.EventEmitter<any>();
+    readonly onDidChangeTreeData: vscode.Event<any> = this._onDidChangeTreeData.event;
+
+    private repoStateChanges: vscode.Disposable[] = [];
+
     constructor() {
+        this.getRepos();
+    }
+
+    private getApi(): API|null {
+        if (this.gitApi) {
+            return this.gitApi;
+        }
         const gitContainer = vscode.extensions.getExtension<GitExtension>('vscode.git');
         if (gitContainer) {
             const gitExtension = gitContainer.exports;
-            const git = gitExtension.getAPI(1);
-            this.repos = git.repositories;
+            const gitApi = gitExtension.getAPI(1);
+            this.gitApi = gitApi;
+            gitApi.onDidChangeState(() => {
+                this.refresh();
+            });
+            return this.gitApi;
+        }
+        return null;
+    }
+
+    private getRepos() {
+        let api = this.getApi();
+        this.repoStateChanges.forEach(listener => listener.dispose());
+        this.repoStateChanges = [];
+        if (api) {
+            this.repos = api.repositories;
+            this.repoStateChanges = this.repos.map(
+                (repo) => {
+                    return repo.state.onDidChange(() => {
+                        this.refresh();
+                    });
+                }
+            );
         }
     }
 
-    public async getBranches(repo: Repository): Promise<string[]> {
+    public refresh(): any {
+        this.getRepos();
+        this._onDidChangeTreeData.fire();
+    }
+
+    public async getBranches(repo: Repository): Promise<Branch[]> {
         const path = repo.rootUri.fsPath;
         if (!path) {
             return [];
@@ -33,12 +74,50 @@ export class BranchTreeProvider implements vscode.TreeDataProvider<Branch> {
                 cwd: path
             }
         );
-        const branches = stdout.replace(/^\*\s+/g, '').split(/\n/g).filter(branch => !!branch);
+        const branchNames = stdout.split(/\n/g).filter(branch => !!branch);
+        const branches: Branch[] = branchNames.map((branch) => {
+            const isStarred = branch.indexOf('*') === 0;
+            const branchName = isStarred ? branch.slice(1).trim() : branch.trim();
+            return {
+                repo,
+                branchName,
+                selected: isStarred
+            };
+        });
         return branches;
     }
 
+    public async switchBranch(branch: Branch): Promise<void> {
+        const path = branch.repo.rootUri.fsPath;
+        if (!path) {
+            return;
+        }
+        await exec(
+            `git checkout ${branch.branchName}`,
+            {
+                cwd: path
+            }
+        );
+
+        this.refresh();
+    }
+
     getTreeItem(element: Branch): vscode.TreeItem {
-        return new vscode.TreeItem(element.branchName);
+        if (element.branchName) {
+            const item = new vscode.TreeItem(element.branchName);
+            if (element.selected) {
+                item.label = '✔ ' + item.label;
+            }
+            item.command = {
+                command: 'scm-local-branches.switchBranch',
+                arguments: [element],
+                title: 'Switch branch'
+            };
+            return item;
+        }
+        const repoPath = element.repo.rootUri.fsPath;
+        const repoDirectory = repoPath.slice(repoPath.lastIndexOf('/'));
+        return new vscode.TreeItem(repoDirectory);
     }
     async getChildren(element: Branch): Promise<Branch[]> {
         if (!this.repos) {
@@ -48,9 +127,8 @@ export class BranchTreeProvider implements vscode.TreeDataProvider<Branch> {
             // root level
             if (this.repos.length === 1) {
                 const repo = this.repos[0];
-                const branches = await this.getBranches(repo);
 
-                return branches.map(branchName => <Branch>{branchName: branchName});
+                return await this.getBranches(repo);
             }
         }
         return [];
@@ -64,10 +142,11 @@ export class BranchSwitcher {
     constructor(context: vscode.ExtensionContext) {
         const treeDataProvider = new BranchTreeProvider();
         this.scmBranches = vscode.window.createTreeView('scm-local-branches', { treeDataProvider });
-        // vscode.commands.registerCommand('fileExplorer.openFile', (resource) => this.openResource(resource));
+        vscode.commands.registerCommand('scm-local-branches.refresh', () => {
+            treeDataProvider.refresh();
+        });
+        vscode.commands.registerCommand('scm-local-branches.switchBranch', (element: Branch) => {
+            treeDataProvider.switchBranch(element);
+        });
     }
-
-    // private openResource(resource: vscode.Uri): void {
-    //     vscode.window.showTextDocument(resource);
-    // }
 }
